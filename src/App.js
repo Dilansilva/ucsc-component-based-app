56ab53bbf32f21011157e5b2ddfc972c96333b34
UpendraR <upendrar@kasperworld.com>
2023-04-25 13:46:01 +0530

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import { EyeTwoTone,EyeInvisibleOutlined  } from '@ant-design/icons';
+import { Input, Space,Button } from 'antd';
+
+import './App.css'
 
 function App() {
+  const [password,setPassword] = useState(null);
+  const [username,setUsername] = useState(null);
+
+  const submitCredentails = () => {
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        "username":username,
+        "password":password
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+       .then((response) => response.json())
+       .then((data) => {
+          console.log("kmkknk",data);
+          // Handle data
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+  }
+
+  const [credentails,setCredentials] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='textCenter'>
+      <h1>Login</h1>
+      <Space direction="vertical">
+        <Input 
+          placeholder="username" 
+          onChange={(e) => {setUsername(e.target.value)}}
+        />
+        <Input.Password
+          placeholder="password"
+          iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+          onChange={(e) => {setPassword(e.target.value)}}
+        />
+      <Button type="primary" disabled={username && password ? false : true} onClick={submitCredentails}>
+        Login
+      </Button>
+        <small className='errorMsg'>{credentails ? null : 'wrong password or username'}</small>
+      </Space>
     </div>
   );
 }
