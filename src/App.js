becb5e591a58c3b23c11a810fb1e9f8b5abe8674
UpendraR <sdilansilva@gmail.com>
2023-04-27 11:37:18 +0530

    router links changes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { EyeTwoTone,EyeInvisibleOutlined  } from '@ant-design/icons';
-import { Input, Space,Button } from 'antd';
+import {Route,Routes} from "react-router-dom";
+import Login from "./pages/Login"
 
 import './App.css'
 
 function App() {
-
-  const [password,setPassword] = useState(null);
-  const [username,setUsername] = useState(null);
-  const [credentails,setCredentials] = useState(null);
-
-  const submitCredentails = () => {
-    fetch('https://kasper-backend.onrender.com/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        "username":username,
-        "password":password
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-       .then((response) => response.json())
-       .then((data) => {
-          console.log("kmkknk",data.message);
-          if(data.message === "loginsucess"){
-            window.location.replace("https://ant.design/");
-          }
-          setCredentials(data.message)
-       })
-       .catch((err) => {
-          console.log(err.message);
-       });
-  }
-
   return (
-    <div className='textCenter'>
-      <h1>Login</h1>
-      <Space direction="vertical">
-        <Input 
-          placeholder="username" 
-          onChange={(e) => {setUsername(e.target.value)}}
-        />
-        <Input.Password
-          placeholder="password"
-          iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-          onChange={(e) => {setPassword(e.target.value)}}
-        />
-      <Button type="primary" disabled={username && password ? false : true} onClick={submitCredentails}>
-        Login
-      </Button>
-        <small className='errorMsg'>{credentails ? credentails : null}</small>
-      </Space>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route/>
+    </Routes>
   );
 }
 
